9512d72c1e06e1a49919169acf21eb9e073b424b
Roberto Gamboni <cproby2005@gmail.com>
2010-10-31 23:34:39 +0100

    zombies sample, comments and notes on slides

---
+++ objc/samples/10.zombies.m
@@ -0,0 +1,48 @@
+#import <Foundation/Foundation.h>
+
+@interface ParkingLot : NSObject
+{
+	NSMutableArray* slots;
+}
+
+- (void)fillSlot:(int) index;
+- (void)badStuff;
+
+@end
+
+@implementation ParkingLot
+
+- (id) init {
+	if(self = [super init]) {
+		slots = [[NSMutableArray alloc] initWithCapacity:10];
+	}
+	return self;
+}
+- (void)fillSlot:(int) index {
+	[slots addObject:[NSNumber numberWithInt:index]];
+	printf("I filled %d spots!\n", (int)slots.count);
+}
+- (void)badStuff {
+	[slots release];
+}
+
+@end
+
+int main (int argc, const char * argv[]) {
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	printf("Hello world.\n");
+	
+	ParkingLot* parking = [[ParkingLot alloc] init];
+	[parking fillSlot:4];
+	[parking fillSlot:1];
+	[parking fillSlot:2];
+	
+	[parking badStuff]; //here goes the release
+	
+	[parking fillSlot:7];
+	[parking fillSlot:9];
+	
+	[pool drain];
+	return 0;
+}
+
